68b96b5d3048c9f52924a5f25f3c86040669d70b
T8mpest <opcache@gmail.com>
2023-06-12 15:05:19 +0300

    first try use my knowledge

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+var strValue = Console.ReadLine();  
+var val = int.Parse(strValue);
+var condition = val >= 1 && val / val == 1;
+if (condition)
+{
+    Console.WriteLine($" {val} is a prime number ");
+}
+else
+{
+    Console.WriteLine($"{val} is NOT prime number");
+}
+Console.ReadLine();
+
